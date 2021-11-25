da162edab0a6b1113f08dc3c3094f66c7228ccff
azizmuratk <azizkaraca0@gmail.com>
2021-11-25 23:11:26 +0300

    GET/POST/PUT/DELETE actions

---
+++ UserManagement/UserManagement/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models
+{
+    public class User
+    {
+        //user information defined
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [MinLength(4), MaxLength(12)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
